0917287e55e421925aa6d3896d81f11450297098
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-02-26 19:14:53 -0300

    Adicionar modelo do vosk

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+
 # Our main file
+
+# reconhecimento de voz online pelo google
+'''
 import speech_recognition as sr 
 
 # Cria um reconhecedor
@@ -10,7 +14,59 @@ with sr.Microphone() as source:
         audio = r.listen(source) # Define microfone como fonte de áudio
 
         print(r.recognize_google(audio, language='pt')) #on line
+'''
 
 
+
+
+
+#!/usr/bin/env python3
+
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
+import pyttsx3
+import json
+import core
+from nlu.classifier import classify
+# Síntese de fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+# Reconhecimento de fala
+
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
+stream.start_stream()
+
+# Loop do reconhecimento de fala
+while True:
+    data = stream.read(2048)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        result = rec.Result()
+        result = json.loads(result)
+
+        if result is not None:
+            text = result['text']
+
+            # Reconhecer entidade do texto.
+            entity = classify(text)
+
+            if entity == 'time\getTime':
+                speak(core.SystemInfo.get_time())
+
+            print('Text: {}  Entity: {}'.format(text, entity))
 
 
